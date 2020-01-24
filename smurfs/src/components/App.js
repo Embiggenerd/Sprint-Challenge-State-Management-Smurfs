ede695233f211eced375cfc4d240a2d0f84f1aee
igor <embiggenerd@gmail.com>
2020-01-24 11:59:40 -0500

    getting smurfs on mount

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from "react";
+import React, { useEffect } from "react";
 import "./App.css";
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <h1>SMURFS! 2.0 W/ Redux</h1>
-        <div>Welcome to your state management version of Smurfs!</div>
-        <div>Start inside of your `src/index.js` file!</div>
-        <div>Have fun!</div>
-      </div>
-    );
-  }
+import axios from 'axios'
+
+const App = () => {
+  useEffect(() => {
+    const getSmurfs = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:3333/smurfs')
+        console.log('getSmurfs data', data)
+      } catch (error) {
+        console.log('getSmurfs error', error)
+      }
+    }
+    getSmurfs()
+  }, [])
+
+
+
+  return (
+    <div className="App">
+      <h1>SMURFS! 2.0 W/ Redux</h1>
+      <div>Welcome to your state management version of Smurfs!</div>
+      <div>Start inside of your `src/index.js` file!</div>
+      <div>Have fun!</div>
+    </div>
+  );
+
 }
 
 export default App;
